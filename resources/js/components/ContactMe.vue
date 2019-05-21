11be4bd76e173545553b92e72f0ced80ab24ee84
Junhai <junhai@koreconx.com>
2019-05-20 23:07:47 -0400

    Add Vessel Docker configs;keep working on contact me

--- resources/js/components/ContactMe.vue
+++ resources/js/components/ContactMe.vue
@@ -3,7 +3,7 @@
         <template #header class="clearfix">
             <h3>Contact Me</h3>
         </template>
-        <el-form ref="form" :model="form" label-position="top">
+        <el-form ref="form" :model="form" label-position="top" @submit.native.stop>
             <el-form-item label="Your Name">
                 <el-input placeholder="placeholder"></el-input>
             </el-form-item>
@@ -16,13 +16,70 @@
                     :rows="4">
                 </el-input>
             </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="submit">Submit</el-button>
+            </el-form-item>
         </el-form>
     </el-card>
 </template>
 
 <script>
     export default {
-        name: "ContactMe"
+        name: "ContactMe",
+        data: () => {
+            return {
+                model: {
+                    name: "",
+                    email: "",
+                    message: ""
+                },
+                rules: {
+                    email: [
+                        {
+                            required: true,
+                            message: "Please Enter your email address",
+                            trigger: "blur"
+                        },
+                        {
+                            type: "email",
+                            message: "Please enter a valid email address",
+                            trigger: "blur"
+                        }
+                    ],
+                    name: [
+                        {
+                            required: true,
+                            message: "Please enter your name",
+                            trigger: "blur"
+                        }
+                    ],
+                    message: [
+                        {
+                            required: true,
+                            message: "Please enter your message",
+                            trigger: "blur"
+                        }
+                    ]
+                }
+            };
+        },
+          methods: {
+            submit () {
+                this.$refs.form.validate((valid) => {
+                    if (valid) {
+                        axios.post("/contact-me")
+                            .then((response) => {
+                                console.log(response);
+                            })
+                            .catch((error) => {
+
+                            });
+                    } else {
+                        Message.error("There are errors on your form, please check");
+                    }
+                });
+            }
+        }
     };
 </script>
 
